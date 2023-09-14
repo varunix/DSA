8476dda747f651f2094f5e38484a96416d89ec5d
Varun Chaubey <varunchaubey4@gmail.com>
2023-09-14 09:48:15 +0530

    Add new way for solving triangle star

--- Patterns/TriangleStar.java
+++ Patterns/TriangleStar.java
@@ -2,19 +2,27 @@ package Patterns;
 
 class TriangleStar {
     public static void printTriangleStar(int n) {
-        int str = 1;
-        int sp = n-1;
-        for(int i = 1; i <= n; i++) {
-            for(int j = 1; j <= str; j++) {
+        // int str = 1;
+        // int sp = n-1;
+        // for(int i = 1; i <= n; i++) {
+        //     for(int j = 1; j <= str; j++) {
+        //         System.out.print("* ");
+        //     }
+
+        //     for(int j = 1; j <= sp; j++) {
+        //         System.out.print(" ");
+        //     }
+
+        //     str++;
+        //     sp--;
+        //     System.out.println();
+        // }
+
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<=i; j++) {
                 System.out.print("* ");
             }
 
-            for(int j = 1; j <= sp; j++) {
-                System.out.print(" ");
-            }
-
-            str++;
-            sp--;
             System.out.println();
         }
     }
