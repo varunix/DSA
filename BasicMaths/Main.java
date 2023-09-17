1dc3de40308617e5898a9f10397b8dfbeaa46ac8
Varun Chaubey <varunchaubey4@gmail.com>
2023-09-17 23:21:37 +0530

    Add reverse integer

--- BasicMaths/Main.java
+++ BasicMaths/Main.java
@@ -3,6 +3,8 @@ package BasicMaths;
 public class Main {
     public static void main(String args[]) {
         CountDigits countDigits = new CountDigits();
+        ReverseInteger reverseInteger = new ReverseInteger();
         countDigits.count_Digits(12345);
+        reverseInteger.reverse_Integer(-123);
     }
 }
---
+++ BasicMaths/ReverseInteger.java
@@ -0,0 +1,15 @@
+package BasicMaths;
+
+public class ReverseInteger {
+    public static void reverse_Integer(int n) {
+        long sum = 0;
+        while(n != 0) {
+            int digit = n%10;
+            sum = sum*10 + digit;
+            n = n/10;
+        }
+
+        if(sum < Integer.MIN_VALUE || sum > Integer.MAX_VALUE) System.out.println(0);
+        else System.out.println("Reverse Integer: " + (int)sum);
+    }
+}
