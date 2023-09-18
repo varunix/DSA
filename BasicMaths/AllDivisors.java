b5788b09e8db1e32eb4912706673fdcce5f9fd18
Varun Chaubey <varunchaubey4@gmail.com>
2023-09-18 22:52:29 +0530

    Add print all divisors

---
+++ BasicMaths/AllDivisors.java
@@ -0,0 +1,24 @@
+package BasicMaths;
+
+public class AllDivisors {
+    public static void printAllDivisors(int n) {
+        System.out.print("All Divisors: ");
+        // for(int i = 1; i <= n; i++) {
+        //     if(n % i == 0) {
+        //         System.out.print(i + " ");
+        //     }
+        // }
+
+        for(int i = 1; i <= (int)Math.sqrt(n); i++) {
+            if(n%i == 0) {
+                System.out.print(i + " ");
+                if(n/i != i) {
+                    System.out.print(n/i + " ");
+                }
+            }
+
+        }
+    }
+}
+
+// Output: 1 2 3 4 6 9 12 18 36 or 1 36 2 18 3 12 4 9 6 (here we are printing divisor and quotient)
--- BasicMaths/Main.java
+++ BasicMaths/Main.java
@@ -5,8 +5,10 @@ public class Main {
         CountDigits countDigits = new CountDigits();
         ReverseInteger reverseInteger = new ReverseInteger();
         Palindrome palindrome = new Palindrome();
+        AllDivisors allDivisors = new AllDivisors();
         countDigits.count_Digits(12345);
         reverseInteger.reverse_Integer(-123);
         palindrome.printPalindrome(-121);
+        allDivisors.printAllDivisors(36);
     }
 }
