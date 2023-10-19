fa04efed4369b601d1cd1c031c81113a33ba2a59
Varun Chaubey <varunchaubey4@gmail.com>
2023-10-19 09:08:20 +0530

    Update longest subarray with give sum k positive with hasing approach

--- Arrays/Easy/LongestSubarrayWithGivenSumKPositives.java
+++ Arrays/Easy/LongestSubarrayWithGivenSumKPositives.java
@@ -1,25 +1,54 @@
 package Arrays.Easy;
 
+import java.util.HashMap;
+
 public class LongestSubarrayWithGivenSumKPositives {
     static int findSum(int[] arr, int N, int k) {
+        //Better Approach (Hashing)
         int maxLen = 0;
-        long sum = arr[0];
-        int left = 0, right = 0;
-        while(right < N) {
-            while(left <= right && sum > k) {
-                sum -= arr[left];
-                left++;
-            }
+        long sum = 0;
+        HashMap<Long, Integer> preSumMap = new HashMap<>();
+        
+        for(int i=0; i<N; i++) {
+            sum += arr[i];
 
             if(sum == k) {
-                maxLen = Math.max(maxLen, right-left+1);
+                maxLen = Math.max(maxLen, i+1);
             }
 
-            right++;
-            if(right < N) sum += arr[right];
+            long rem = sum - k;
+            
+            if(preSumMap.containsKey(rem)) {
+                int len  = i - preSumMap.get(rem);
+                maxLen = Math.max(maxLen, len);
+            }
+
+            if(!preSumMap.containsKey(sum)) {
+                preSumMap.put(sum, i);
+            }
         }
 
         return maxLen;
+
+        //Optimal Approach (2 pointers)
+        // int maxLen = 0;
+        // long sum = arr[0];
+        // int left = 0, right = 0;
+        // while(right < N) {
+        //     while(left <= right && sum > k) {
+        //         sum -= arr[left];
+        //         left++;
+        //     }
+
+        //     if(sum == k) {
+        //         maxLen = Math.max(maxLen, right-left+1);
+        //     }
+
+        //     right++;
+        //     if(right < N) sum += arr[right];
+        // }
+
+        // return maxLen;
     }
 
     static void longestSubarray() {
