6f55f538ef4411561af1028e96ff10f017ac59f0
Varun Chaubey <varunchaubey4@gmail.com>
2023-11-02 09:19:08 +0530

    Print maximum sum continous array using Kadane's algo

--- Arrays/Medium/MaximumSubarraySum.java
+++ Arrays/Medium/MaximumSubarraySum.java
@@ -17,17 +17,44 @@ public class MaximumSubarraySum {
 
     //Using Kadane's Algorithm
     static void maxSum(int[] arr) {
-        int max_sum = arr[0];
+        // int max_sum = arr[0];
+        // int sum = 0;
+        // for(int i=0; i<arr.length; i++) {
+        //     sum += arr[i];
+        //     max_sum = Math.max(max_sum, sum);
+        //     if(sum < 0) {
+        //         sum = 0;
+        //     }
+        // }
+
+        // System.out.println("Maximum Sum:" + max_sum);
+
+        //There might be more than one subarray with the maximum sum. We need to print any of them
+
+        int max_sum = Integer.MIN_VALUE;
         int sum = 0;
+        int start = 0;
+        int ansStart = -1, ansEnd = -1;
         for(int i=0; i<arr.length; i++) {
+            if(sum == 0) start = i;
             sum += arr[i];
-            max_sum = Math.max(max_sum, sum);
+
+            if(sum > max_sum) {
+                max_sum = sum;
+                ansStart = start;
+                ansEnd = i;
+            }
+
             if(sum < 0) {
                 sum = 0;
             }
         }
 
-        System.out.println("Maximum sum:" + max_sum);
+        System.out.print("The subarray is: [");
+        for (int i = ansStart; i <= ansEnd; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]");
     }
 
     public static void maximumSubarraySum() {
